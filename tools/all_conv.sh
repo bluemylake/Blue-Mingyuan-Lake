d00ae70af882f784c5f2a4f175328fbc9dde92eb
Zhao <davidxk@qq.com>
2015-08-04 19:36:52 +0800

    New Tool: converts all csv from tool/ $ cd tools/ $ ./all_conv.sh * If some process dosen't end normally. Just close mingWin

---
+++ tools/all_conv.sh
@@ -0,0 +1,9 @@
+cd ../Resources/csv/
+cd day_plot
+ls | perl ../../../tools/conv.pl 
+cd ../night_plot
+ls | perl ../../../tools/conv.pl 
+cd ../static
+ls | perl ../../../tools/conv.pl 
+cd ../weekday
+ls | perl ../../../tools/conv.pl 
